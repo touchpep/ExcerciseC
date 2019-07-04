6fd09d025eab89c31376e02188e204355bd66dc3
attaporn sophonslidsuk <sophonslidsuk@yahoo.com>
2019-07-04 21:18:09 +0700

    Update 01-03_welcome.c

--- Ch01/01-03_welcome.c
+++ Ch01/01-03_welcome.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-	printf("Welcome to the C Language and This is first time that i change code!\n");
+	printf("Welcome to the C Language and This is first time that i change code! and this is 2nd time from mac pro\n");
 	return(0);
 }
